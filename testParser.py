5dc77648ed95c93bfa84dc68a002e7ab3ee5daa1
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-02-17 09:55:44 +0000

    Modified version : parse tree

--- testParser.py
+++ testParser.py
@@ -1,4 +1,4 @@
-import sys
+import sys,re
 import ply.yacc as yacc
 import ply.lex as lex
 
@@ -17,50 +17,68 @@ contents = f.read()
 f.close()
 
 result = parser.parse(contents)
+'''
+for node in nodes:
+	print node , '\n'
+print '\n\n'
+'''
 
-visited = []
-n = 0
-suff = []
+nonTerm = []
 for i in range(len(nodes)):
-    current = nodes[i]
-    if(type(current['right']) is list):
-        for j in range(len(current['right'])):
-            for k in range(len(visited)):
-                if(current['right'][j] == visited[k]['right']):
-                    current['right'][j] = visited[k]['left']
-    else:
-        for k in range(len(visited)):
-                if(current['right'] == visited[k]['right']):
-                    current['right'].replace(current['right'], visited[k]['left'])
-                    
+    nodes[i]['left'] += '_' + str(i)
+    nonTerm.append(nodes[i]['left'])
 
-    temp = {'left' : current['left'] + str(len(suff)), 'right' : current['right']}
-    suff.append(len(suff))
-    visited.append(temp)
+n = len(nodes)
+for i in range(n):
+	if(type(nodes[n-i-1]['right']) is list):
+		for k in range(n-i-1):
+			if(nodes[n-i-1]['right'] == nodes[k]['right']):
+				nodes[n-i-1]['right'] = nodes[k]['left']
+for i in range(n):
+	if(type(nodes[n-i-1]['right']) is list):
+	    for j in range(len(nodes[n-i-1]['right'])):
+	    	if(type(nodes[n-i-1]['right'][j]) is list):
+		    	for k in range(n-i-1):
+		    		if(nodes[n-i-1]['right'][j] == nodes[k]['right']):
+		    			nodes[n-i-1]['right'][j] = nodes[k]['left']
+
+vis = []
+def iter(i):
+	if(type(nodes[i]['right']) is list ):
+		for j in range(len(nodes[i]['right'])):
+			if(nodes[i]['right'][j] in nonTerm):
+				m = re.findall(r'\d+', nodes[i]['right'][j])
+				iter(int(m[0]))
+			else:
+				for k in range(n):
+					if(k not in vis):
+						if(nodes[i]['right'][j] == nodes[k]['right']):
+							nodes[i]['right'][j] = nodes[k]['left']
+							vis.append(k)
+	elif(nodes[i]['right'] in nonTerm):
+		m = re.findall(r'\d+', nodes[i]['right'])
+		iter(int(m[0]))
+
+
+		    				
+
+
+m = re.findall(r'\d+', nodes[n-1]['left'])
+iter(int(m[0]))
+
+'''
+for node in nodes:
+	print node , '\n'
+'''
 
 code = ''
-n = len(visited)
-for i in range(n):
-    current = visited[n -i -1]
-    if(type(current['right']) is list):
-        for prod in current['right']:
-            code += current['left'] + ' -> ' + '"' + str(prod) + '"' + '\n'
-    else:
-        code += current['left'] + ' -> ' + '"' + current['right'] + '"' + '\n'
+for i in range(len(nodes)):
+	if(type(nodes[n-1-i]['right']) is list):
+		for prod in range(len(nodes[n-1-i]['right'])):
+			code += nodes[n-1-i]['left'] + ' -> ' + '"' + nodes[n-1-i]['right'][prod] + '"' + '\n'
+	if(type(nodes[n-1-i]['right']) is str):
+		code += nodes[n-1-i]['left'] + ' -> ' + '"' + nodes[n-1-i]['right'] + '"' + '\n'
 
-code = code.replace('[','')
-code = code.replace(']','')
-#print(visited)
 f = open('ptree.dot',"w+")
 f.write('digraph G {\n' + code + '\n}')
-
-
-
-
-
-
-
-
-
-
 
