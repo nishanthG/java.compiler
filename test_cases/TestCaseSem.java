4c3e4f91130bc45ae6d36669ea0b7f625919d307
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-03-21 13:41:48 +0000

    TestCaseSem.java created online with Bitbucket

---
+++ test_cases/TestCaseSem.java
@@ -0,0 +1,36 @@
+public class B {
+	int y = 5;
+	{
+		int b;
+		{
+			int d;
+		}
+	}
+	int c;
+	{
+		int e, y;
+		e = y + 1;
+	}
+	int yu;
+	public static void main(String[] args) {
+      int a, bb=1 + 5;
+      float f;
+      a = bb + 2;
+      boolean bool;
+      while(bool){
+      	a = a + 1;
+      }
+      do{
+      	a = a + 1;
+      }while(bool);
+
+   }
+	private static int method(int argu) {
+      int m;
+      int i;
+	  for(i = 0; i < 4 ; i++){
+	  	int a = i;
+	  }
+	  return m;
+   }
+}
