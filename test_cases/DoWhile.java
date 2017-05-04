d499457d3979d61ac3c3e80dfe8645d00bdbcccd
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-05-04 03:38:12 +0000

    DoWhile.java created online with Bitbucket

---
+++ test_cases/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+
+   public static void main(int arg) {
+      int i = 0;
+      int a = 0;
+
+      do {
+         	a = i;
+			System.out.println(a);
+			i = i + 1;
+      }while( i < 5 );
+   }
+}
