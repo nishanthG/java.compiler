778a5fffd2849d527137e403a829ae4f121f71eb
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-05-04 03:41:27 +0000

    Params.java created online with Bitbucket

---
+++ test_cases/Params.java
@@ -0,0 +1,12 @@
+public class Params {
+	public static int add1(int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8){
+		return a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8;
+	}
+	public static void main() {
+		int x = 0;
+      	x = add1(1, 2, 3, 4, 5, 6, 7, 8);
+        System.out.println(x);
+
+   }
+	
+}
