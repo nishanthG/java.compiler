c4e40ba18a4ca9afe73c7de405787bbcfb72d4c5
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-05-04 03:42:40 +0000

    While.java created online with Bitbucket

---
+++ test_cases/While.java
@@ -0,0 +1,11 @@
+public class While {
+
+   public static void main(String[] args) {
+      int x = 0;
+
+      while( x < 10 ) {
+         x = x + 1;
+         System.out.println(x);
+      }
+   }
+}
