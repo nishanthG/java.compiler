ee12b3e8050ee11c79198900ae8589291f740108
nishanth gunupudi <nishanth.gunupudi@gmail.com>
2017-05-04 03:40:09 +0000

    IfElse.java created online with Bitbucket

---
+++ test_cases/IfElse.java
@@ -0,0 +1,13 @@
+public class IfElse {
+
+   public static void main(String[] args) {
+      int x = 5;
+
+      if( x < 10 ) {
+         System.out.println(x);
+      }
+      else{
+         System.out.println(x+1);
+      }
+   }
+}
